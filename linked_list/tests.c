486aa47084e98a10c6e87f271d773b3d42077bcf
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-04-20 19:47:18 -0400

    contains_data tests.

--- linked_list/tests.c
+++ linked_list/tests.c
@@ -36,10 +36,60 @@ char *test_append_data_links_refs(void) {
 	return 0;
 }
 
+char *test_contains_data_not_present(void) {
+	struct Node *head = node_create(5);
+
+	append_data(head, 10);
+	append_data(head, 20);
+
+	MU_ASSERT("list does not contain data", contains_data(head, 15) == false);
+
+	free(head->next->next);
+	free(head->next);
+	free(head);
+	return 0;
+}
+
+char *test_contains_data_present(void) {
+	struct Node *head = node_create(5);
+
+	MU_ASSERT("list does not contain data 10", contains_data(head, 10) == false);
+	append_data(head, 10);
+	MU_ASSERT("list contains data 10", contains_data(head, 10) == true);
+
+	MU_ASSERT("list does not contain data 15", contains_data(head, 15) == false);
+	append_data(head, 15);
+	MU_ASSERT("list contains data 15", contains_data(head, 15) == true);
+
+	free(head->next->next);
+	free(head->next);
+	free(head);
+	return 0;
+}
+
+char *test_contains_data_one_node_true(void) {
+	struct Node *head = node_create(5);
+	MU_ASSERT("single node contains data", contains_data(head, 5) == true);
+	free(head);
+	return 0;
+}
+
+char *test_contains_data_one_node_false(void) {
+	struct Node *head = node_create(5);
+	MU_ASSERT("single node contains data", contains_data(head, 10) == false);
+	free(head);
+	return 0;
+}
+
 /* Test Suite */
 char *test_suite(void) {
 	MU_RUN_TEST(test_set_node_members);
 	MU_RUN_TEST(test_append_data_links_refs);
+
+	MU_RUN_TEST(test_contains_data_not_present);
+	MU_RUN_TEST(test_contains_data_present);
+	MU_RUN_TEST(test_contains_data_one_node_true);
+	MU_RUN_TEST(test_contains_data_one_node_false);
     return 0;
 }
 
