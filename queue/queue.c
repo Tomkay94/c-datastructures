e6a2accfa43c341960e7615261654e38186c6790
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-04-20 00:38:02 -0400

    Queue enqueue, dequeue, show.

--- queue/queue.c
+++ queue/queue.c
@@ -3,31 +3,70 @@
 #include "queue.h"
 
 struct Queue {
-	int capacity;
 	int size;
-	int *front;
-	int *back;
+	struct Node *front;
+	struct Node *back;
 };
 
-struct Queue* queue_create(int cap) {
+struct Node {
+	int data;
+	struct Node *next;
+};
+
+struct Queue* queue_create() {
 	struct Queue *q = malloc(sizeof(struct Queue));
-	q->capacity = cap;
 	q->size = 0;
-	q->front = NULL;
-	q->back = NULL;
+	q->front = malloc(sizeof(struct Node));
+	q->back = malloc(sizeof(struct Node));
 	return q;
 }
 
-void q_enqueue(struct Queue *queue, int data) {
+struct Node* node_create(int d) {
+	struct Node *n = malloc(sizeof(struct Node));
+	n->data = d;
+	n->next = NULL;
+	return n;
+}
+
+void q_enqueue(struct Queue *q, int data) {
+	struct Node *head = node_create(data);
+
+	if (q->size == 0) {
+		q->front = head;
+	}
+
+	else {
+		q->back->next = head;
+	}
+
+	q->back = head;
+	++(q->size);
 	return;
 }
 
-void q_dequeue(struct Queue *queue) {
+void q_dequeue(struct Queue *q) {
+
+	if (q->size > 0) {
+		q->front = q->front->next;
+		--(q->size);
+	}
 	return;
 }
 
-bool q_is_empty(struct Queue *queue) {
-	return (queue->size == 0);
+void q_show(struct Queue *q) {
+	if (q->size > 0) {
+		struct Node *curr = q->front;
+    	while(curr != NULL) {
+	    	printf("%i ", curr->data);
+        	curr = curr->next;
+    	}
+    	printf("\n");
+	}
+	return;
+}
+
+bool q_is_empty(struct Queue *q) {
+	return (q->size == 0);
 }
 
 #ifdef NOT_TESTING
