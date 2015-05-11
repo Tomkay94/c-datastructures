f33ac046054151be1915eb1ab35b6144c22f294c
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-05-10 23:26:32 -0400

    Begin hashmap.

--- hashmap/hashmap.h
+++ hashmap/hashmap.h
@@ -2,12 +2,17 @@
  * Hash map data type.
  */
 struct HashMap {
-	int *map_array;
-	int capacity;
-	int size;
+	struct HashNode *map;
+	unsigned int capacity;
+	unsigned int size;
 }
 
-/*
- * Hash map functions.
- */
+struct HashNode {
+	char *data;
+}
+
+ /*
+  * Hash map functions.
+  */
 struct HashMap *hash_map_create(int capacity);
+struct HashMap *hash_insert(struct HashMap *hm, char *data);
